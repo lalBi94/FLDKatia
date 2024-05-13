46c9cf12ab6d3e39eb97254b82e30d546bf57da8
pro.boooooo <pro.boudjemline@gmail.com>
2024-05-13 21:30:49 +0200

    [ASKING AUTO-DEPLOY]

--- src/pages/Customer/Categories/Admin/Forms/Items/CreateItem.jsx
+++ src/pages/Customer/Categories/Admin/Forms/Items/CreateItem.jsx
@@ -110,7 +110,6 @@ export default function CreateItem({ handleClose }) {
         }).then((res) => {
             console.log(res);
             setStatus(res.status);
-            setImgRef(null);
         });
     };
 
